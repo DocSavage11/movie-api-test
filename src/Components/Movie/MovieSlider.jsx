a2d3e7b8b93a14212126dfd72b5e28127cd3d002
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-14 22:38:28 -0500

    First commit, obviously

---
+++ src/Components/Movie/MovieSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Movie.css'
+
+import MovieCard from './MovieCard'
+
+function MovieSlider() {
+  return (
+    <MovieCard/>
+  )
+}
+
+export default MovieSlider
